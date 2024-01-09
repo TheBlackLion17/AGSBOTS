d8bf35cb24d8ddb6d59afdab04f6aa248ccc5ac1
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-01-09 20:13:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,9 +11,9 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['20919286'])
-API_HASH = environ['57b85f72104db3f08f9795b0410eb556']
-BOT_TOKEN = environ['6900955318:AAFW2S6dhijU5ybVSH9qoPi-xucjBqDTqzE']
+API_ID = int(environ('API_ID','20919286'))
+API_HASH = environ('API_HASH','57b85f72104db3f08f9795b0410eb556')
+BOT_TOKEN = environ('BOT_TOKEN','6900955318:AAFW2S6dhijU5ybVSH9qoPi-xucjBqDTqzE')
 
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
